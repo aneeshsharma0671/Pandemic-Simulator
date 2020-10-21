53480a8212e756ea3b1922f1feba5074e067ba9f
Aneesh sharma <aneeshsharma0671@gmail.com>
2020-10-21 13:47:31 +0530

    Modified Nav mesh

--- Assets/AgentController.cs
+++ Assets/AgentController.cs
@@ -7,16 +7,59 @@ public class AgentController : MonoBehaviour
 
     public NavMeshAgent agent;
 
-    // Update is called once per frame
+    public Transform[] Buildings;
+    public Transform[] Houses;
+    private Vector3 lastpos;
+    private float t_copy;
+    private float p=0;
+    private int random;
+    private float t;
+
+    public GameObject GameManager;
+
+    private void Start()
+    {
+        agent.SetDestination(Houses[0].position);
+        lastpos = Houses[0].position;
+    }
+
     void Update()
     {
+        t = GameManager.GetComponent<TimeManager>().t;
+
+        if(t% Mathf.RoundToInt(Random.Range(30.0f, 60.0f)) == 0)
+        {
+            t_copy = t;
+            p = p + 1;
+            random =Mathf.RoundToInt(Random.Range(0.0f , 1.0f));
+            Debug.Log(random);
+            Debug.Log(t_copy);
+          
+        }
+        if (t > t_copy)
+        {
+            if (p % 2 == 0)
+            {
+                agent.SetDestination(Buildings[random].position);
+            }
+            else
+            {
+                agent.SetDestination(Houses[random].position);
+            }
+        }
+
+
+
+
+
+
         if (Input.GetMouseButtonDown(0))
         {
          Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit))
             {
-                agent.SetDestination(hit.point);
+              // agent.SetDestination(hit.point);
             }
         }
     }
---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    [Range(0.1f, 5)]
+    public float modifiedScale;
+
+    public float t;
+
+    private void Start()
+    {
+        t = 0;
+        modifiedScale = 1;
+    }
+    void FixedUpdate()
+    {
+        t =Time.fixedTime;
+
+    }
+    private void Update()
+    {
+        Time.timeScale = modifiedScale;
+    }
+
+}
